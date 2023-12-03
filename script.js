ce9999cfb171bfc9f9e3d50100e476b99bf12c7f
kariray <kari_578@naver.com>
2023-12-03 21:58:28 +0900

    another user name css

--- script.js
+++ script.js
@@ -4,30 +4,37 @@ const messageForm = document.getElementById("send-container");
 const messageInput = document.getElementById("message-input");
 
 const username = prompt("what is your name?");
-appendMessage("You joined");
+appendMessage("You joined", "user-connection");
 socket.emit("new-user", username);
 
 socket.on("chat-message", (data) => {
-  appendMessage(`${data.name}:${data.message}`);
+  appendMessage(`${data.message}`, "message another-message");
 });
 
 socket.on("user-connected", (name) => {
-  appendMessage(`${name} connected`);
+  appendMessage(`${name} connected`, "user-connection");
 });
 socket.on("user-disconnected", (name) => {
-  appendMessage(`${name} disconnected`);
+  appendMessage(`${name} disconnected`, "user-disconnection");
 });
 
 messageForm.addEventListener("submit", (event) => {
   event.preventDefault();
   const message = messageInput.value;
-  appendMessage(`You:${message}`);
+  appendMessage(`${message}`, "message my-message");
   socket.emit("send-chat-message", message);
   messageInput.value = "";
 });
 
-function appendMessage(message) {
+function appendMessage(message, className) {
   const messageElement = document.createElement("div");
   messageElement.innerText = message;
+  messageElement.setAttribute("class", className);
   messageContainer.append(messageElement);
+  if (className.includes("another-message")) {
+    const userElement = document.createElement("span");
+    userElement.innerText = message;
+    userElement.setAttribute("class", "user-name");
+    messageElement.append(userElement);
+  }
 }
